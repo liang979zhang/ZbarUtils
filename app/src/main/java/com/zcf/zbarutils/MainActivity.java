30cddd5af00ec7e81cbbcff90e994b4d2ae65a6f
liang979zhang <896672661@qq.com>
2018-08-12 20:37:49 +0800

    二维码扫描功能

---
+++ app/src/main/java/com/zcf/zbarutils/MainActivity.java
@@ -0,0 +1,53 @@
+package com.zcf.zbarutils;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import cn.bertsir.zbar.QrConfig;
+import cn.bertsir.zbar.QrManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void viewclick(View view) {
+
+        QrConfig qrConfig = new QrConfig.Builder()
+                .setDesText("(识别二维码)")//扫描框下文字
+                .setShowDes(false)//是否显示扫描框下面文字
+                .setShowLight(true)//显示手电筒按钮
+                .setShowTitle(true)//显示Title
+                .setShowAlbum(true)//显示从相册选择按钮
+                .setCornerColor(Color.WHITE)//设置扫描框颜色
+                .setLineColor(Color.WHITE)//设置扫描线颜色
+                .setLineSpeed(QrConfig.LINE_MEDIUM)//设置扫描线速度
+                .setScanType(QrConfig.TYPE_QRCODE)//设置扫码类型（二维码，条形码，全部，自定义，默认为二维码）
+                .setScanViewType(QrConfig.SCANVIEW_TYPE_QRCODE)//设置扫描框类型（二维码还是条形码，默认为二维码）
+                .setCustombarcodeformat(QrConfig.BARCODE_I25)//此项只有在扫码类型为TYPE_CUSTOM时才有效
+                .setPlaySound(true)//是否扫描成功后bi~的声音
+//                .setDingPath(R.raw.test)//设置提示音(不设置为默认的Ding~)
+                .setIsOnlyCenter(true)//是否只识别框中内容(默认为全屏识别)
+                .setTitleText("扫描二维码")//设置Tilte文字
+                .setTitleBackgroudColor(Color.TRANSPARENT)//设置状态栏颜色
+                .setTitleTextColor(Color.BLACK)//设置Title文字颜色
+                .create();
+        QrManager.getInstance().init(qrConfig).startScan(this, new QrManager.OnScanResultCallback() {
+            @Override
+            public void onScanSuccess(String result) {
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+            }
+        }, new QrManager.LeadTextCallBack() {
+            @Override
+            public void clickOk() {
+
+            }
+        });
+    }
+}
